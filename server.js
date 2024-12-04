7186cab7dd10b20b00064a71ac1c86ae9fb96bba
Michael Taheri <michael.taheri33@gmail.com>
2024-12-04 12:28:54 -0800

    created server and set up index route

---
+++ server.js
@@ -0,0 +1,25 @@
+const cors = require('cors');
+const express = require('express');
+
+// These are settings for the server.
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// These are routers that handle different resources.
+const indexRouter = require('./routes/index/index')
+
+app.use('/', indexRouter);
+
+// This runs when an unknown route and HTTP method combination is
+// received.
+app.use((req, res) => {
+  const message = (
+    `There is no route ${req.url} that supports a ${req.method} request.`
+  );
+  res.status(404).send(message);
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});

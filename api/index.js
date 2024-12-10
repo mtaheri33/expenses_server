83a5e779845c3f7437e1e5336887633d94598b99
Michael Taheri <michael.taheri33@gmail.com>
2024-12-09 19:51:18 -0800

    added / route since Vercel sends a request to it and displays the result

--- api/index.js
+++ api/index.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => res.status(200).send('Expenses Server'));
+
 // These are routers that handle different resources.
 const signInRouter = require('./routes/signIn')
 app.use('/signIn', signInRouter);

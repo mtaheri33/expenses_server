3237b7ae71e7e75f1e16d33b33d608bc95d281bb
Michael Taheri <michael.taheri33@gmail.com>
2024-12-23 14:37:52 -0800

    removed redundant comments

--- api/index.js
+++ api/index.js
@@ -3,19 +3,15 @@
 const cors = require('cors');
 const express = require('express');
 
-// These are settings for the server.
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.status(200).send('Expenses Server'));
 
-// These are routers that handle different resources.
 const signInRouter = require('./routes/signIn')
 app.use('/signIn', signInRouter);
 
-// This runs when an unknown route and HTTP method combination is
-// received.
 app.use((req, res) => {
   const message = (
     `There is no route ${req.url} that supports a ${req.method} request.`
